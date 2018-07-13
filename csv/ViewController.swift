f197b5b3e723432d68538e1b78740a3fb1b6a9fa
Maxim Kholmansky <maximkholmansky@maxims-macbook-pro.local>
2018-07-13 09:29:39 +0700

    initial commit

--- csv/ViewController.swift
+++ csv/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class Task: NSObject {
+    var date: String = ""
+    var name: String = ""
+    var startTime: String = ""
+    var endTime: String = ""
+}
 
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+
+    var taskArr = [Task]()
+    var task: Task!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func createReport(_ sender: UIButton) {
+        task = Task()
+        for _ in 0..<5 {
+            task.name = "Raj"
+            task.date = "\(Date())"
+            task.startTime = "Start \(Date())"
+            task.endTime = "End \(Date())"
+            taskArr.append(task!)
+        }
+        creatCSV()
     }
-
-
+    
+    // MARK: CSV file creating
+    func creatCSV() -> Void {
+        let fileName = "Report.csv"
+        let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(fileName)
+        var csvText = "Date,Task Name,Time Started,Time Ended\n"
+        
+        for task in taskArr {
+            let newLine = "\(task.date),\(task.name),\(task.startTime),\(task.endTime)\n"
+            csvText.append(newLine)
+        }
+        
+        do {
+            try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("Failed to create file")
+            print("\(error)")
+        }
+        print(path ?? "not found")
+    }
 }
+
 
